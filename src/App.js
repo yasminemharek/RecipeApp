5964092d04f6df29d8b4da30be3853cbc6abd47b
Yasser Akchayat <akchayaty333@gmail.com>
2024-06-05 15:38:29 +0100

    1

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const App = () => {
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
   const [query, setQuery] = useState("chicken");
+  
 
   useEffect(() => {
     getRecipes();
